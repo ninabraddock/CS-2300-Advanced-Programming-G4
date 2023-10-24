f557f3ba87ce1b13cbc5f43482c27216c1e7ab78
skyler heininger <122641443+SkylerHeininger@users.noreply.github.com>
2023-10-24 10:40:36 -0400

    Update even_odd_site.py

--- programming files/even_odd_site.py
+++ programming files/even_odd_site.py
@@ -1,3 +1,4 @@
+# Bugs introduced: SH
 with open("numbers.html", "w") as f:
     # Write header to file
     f.write("<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n")
@@ -5,13 +6,13 @@ with open("numbers.html", "w") as f:
     # Iterate through 50 integers
     for i in range(1, 50):
         # Write even numbers
-        if i % 2 == 0:
+        if i % 1 == 0: # Should be 2
             f.write("<tr><td>{}</td><td></td></tr>\n".format(i))
         # Write odd numbers
         else:
             f.write("<tr><td></td><td>{}</td></tr>\n".format(i))
     f.write("</table>\n</body>\n</html>")
-
+exit(-1) # Early exit
 # Read from numbers
 with open("numbers.html", "r") as f:
     print(f.read())
