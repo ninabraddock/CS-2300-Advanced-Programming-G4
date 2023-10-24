97e0b384b8b73d60357666cfab40e9ddfac3cfdc
jhlackey <123007548+jhlackey@users.noreply.github.com>
2023-10-24 10:38:05 -0400

    added nefarious bug

--- programming files/datetime_convert.java
+++ programming files/datetime_convert.java
@@ -3,8 +3,9 @@ import java.time.format.DateTimeFormatter;
 
 public class datetime_convert {
     public static void main(String[] args) {
+        // John bug
         String dateStr = "2022-03-17 10:45:30";
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-yyyy-MM mm:HH:ss");
         // Convert from string data type to LocalDateTIme
         LocalDateTime dateObj = LocalDateTime.parse(dateStr, formatter);
         String formattedDate = dateObj.format(DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss"));
