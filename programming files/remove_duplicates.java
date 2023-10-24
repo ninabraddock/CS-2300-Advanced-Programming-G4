a1611e5dce768fc0dfb2ed60e73efa68a5c38a39
jtomasi3 <123200835+jtomasi3@users.noreply.github.com>
2023-10-24 10:40:17 -0400

    Bugs remove_duplicates.java

--- programming files/remove_duplicates.java
+++ programming files/remove_duplicates.java
@@ -7,7 +7,7 @@ public class remove_duplicates {
     public static List<Integer> removeDuplicates(List<Integer> arr, List<Integer> removedElements) {
         List<Integer> uniqueArr = new ArrayList<>();
         for (int i = 0; i < arr.size(); i++) {
-            if (!uniqueArr.contains(arr.get(i))) {
+            if (uniqueArr.contains(arr.get(i))) {
                 uniqueArr.add(arr.get(i));
             } else {
                 removedElements.add(arr.get(i));
