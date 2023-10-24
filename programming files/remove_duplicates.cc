1228f9f3d95dd76ca8dc5a9856ca66f494662440
kirasafferstein <95302748+kirasafferstein@users.noreply.github.com>
2023-10-24 10:25:17 -0400

    Update remove_duplicates.cc

--- programming files/remove_duplicates.cc
+++ programming files/remove_duplicates.cc
@@ -4,17 +4,23 @@
 using namespace std;
 
 vector<int> remove_duplicates(vector<int> arr, vector<int>& removed_elements){
+    // Initialize vector of ints
     vector<int> unique_arr;
+    // Sort the vector
     sort(arr.begin(), arr.end());
     int n = arr.size();
+    // Loop through the sorted vector to find duplicates
     for(int i=0; i<n; i++){
+        // If its a unique element, add it to the unique_arr vector
         if(i == 0 || arr[i] != arr[i-1]){
             unique_arr.push_back(arr[i]);
         }
         else{
+            //If its a duplicate add it to the removed_elements vector
             removed_elements.push_back(arr[i]);
         }
     }
+    // Return the sorted vector
     return unique_arr;
 }
 
@@ -28,11 +34,13 @@ int main(){
         cout << arr[i] << " ";
     }
     cout << endl;
+    // Output the unique elements
     cout << "Unique array: ";
     for(int i=0; i<unique_arr.size(); i++){
         cout << unique_arr[i] << " ";
     }
     cout << endl;
+    // Output the duplicates
     cout << "Removed elements: ";
     for(int i=0; i<removed_elements.size(); i++){
         cout << removed_elements[i] << " ";
