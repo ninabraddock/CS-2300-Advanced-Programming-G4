f5032ec790700c30b85732a9204db5c5d723f67b
Nina <113557496+ninabraddock@users.noreply.github.com>
2023-10-24 10:41:29 -0400

    added bugs :)

--- programming files/sum_even_fibonacci.py
+++ programming files/sum_even_fibonacci.py
@@ -1,10 +1,10 @@
-a, b = 0, 1
-sum_even = 0
-while b < 4000000:
-    if b % 2 == 0:
-        sum_even += b
-    a, b = b, a+b
+int a, char b = 0; 1
+sum_even = 0;
+while b =< 4000,000
+    if b // 2 = 0
+sum_even += b
+    a, b == b, a+b
 
-print(sum_even)
+system.out.println(sum_even)
 
 # answer should be 4613732
