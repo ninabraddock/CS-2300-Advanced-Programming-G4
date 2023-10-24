60dcbace3827bab756e8498bc3a202e54cbe9d6f
jhlackey <123007548+jhlackey@users.noreply.github.com>
2023-10-24 10:40:39 -0400

    added bug

--- programming files/datetime_convert.cc
+++ programming files/datetime_convert.cc
@@ -5,10 +5,12 @@
 
 int main()
 {
+
+    // John added bugs
     std::string date_str = "2022-03-17 10:45:30";
     std::tm date_obj = {};
     std::istringstream ss(date_str);
-    ss >> std::get_time(&date_obj, "%Y-%m-%d %H:%M:%S");
+    ss >> std::get_time(date_obj, "%Y-%m-%d %H:%M:%S");
     std::stringstream formatted_date_ss;
     // Push string to stringstream
     formatted_date_ss << std::put_time(&date_obj, "%m/%d/%Y %H:%M:%S");
